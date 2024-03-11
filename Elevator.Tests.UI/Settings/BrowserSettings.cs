1d5319547e5c78540206c0006ef65132dcf665d0
Panagiotis Meretis <meretis.panagiotis@ground-control.co.uk>
2024-03-11 17:13:07 +0000

    arrangements to set the correct files

--- Elevator.Tests.UI/Settings/BrowserSettings.cs
+++ Elevator.Tests.UI/Settings/BrowserSettings.cs
@@ -5,6 +5,5 @@ namespace Practice.One.UI.Settings.Configuration
     {
         public int PageLoadTimeout { get; set; } = 3000;
         public int ScriptTimeout { get; set; } = 1000;
-        public int ArtificialDelayBeforeAction { get; set; } = 0;
     }
 }
--- Elevator.Tests.UI/Settings/ConfigurationService.cs
+++ Elevator.Tests.UI/Settings/ConfigurationService.cs
@@ -17,16 +17,10 @@ namespace Practice.One.UI.Settings
         public static TSection GetSection<TSection>()
             where TSection : class, new()
         {
-            string sectionName = MakeFirstLetterToLower(typeof(TSection).Name);
-
+            string sectionName = typeof(TSection).Name;
             return Root.GetSection(sectionName).Get<TSection>();
         }
-
-        private static string MakeFirstLetterToLower(string text)
-        {
-            return char.ToLower(text[0]) + text.Substring(1);
-        }
-
+      
         public static WebSettings GetWebSettings()
         {
             var result = Root.GetSection("websettings").Get<WebSettings>();
